f679948ca703a1ffe5ddc814f3c7c7b62942b60b
Vulpoiu Catalin <vulpoiucatalin@gmail.com>
2025-03-28 21:11:55 +0200

    Actualizare configurație pentru GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Cosmic-trader/',
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    emptyOutDir: true
   },
   server: {
     port: 3000,
